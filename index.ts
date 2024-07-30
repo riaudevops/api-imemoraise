6d3f2d2b6fc3bf07e6549293c89bc3f49679bfb0
DIKI-WAHYUDI-2 <muhammaddikiw2@gmail.com>
2024-07-30 12:25:15 +0700

    🔥feat: adding controller for dosen

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+import mahasiswaRoutes from "./src/routes/mahasiswa.routes";
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(mahasiswaRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
