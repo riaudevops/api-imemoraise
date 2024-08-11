14c9c311ac324de42a79df186a8a29a2ed0bc014
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-12 01:07:31 +0700

    🔥 feat: applying dosen.routes at main entry files

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import mahasiswaRoutes from "./src/routes/mahasiswa.routes";
+import dosenRoutes from "./src/routes/dosen.routes";
 
 const app = express();
 const port = 5000;
@@ -9,6 +10,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(dosenRoutes);
 app.use(mahasiswaRoutes);
 
 app.listen(port, () => {
