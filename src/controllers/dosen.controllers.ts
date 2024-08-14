2edd0fd3d40a7a3e55dc70ac31f04daee6c6c30a
DIKI-WAHYUDI-2 <muhammaddikiw2@gmail.com>
2024-08-14 16:57:04 +0700

    🔥 feat: add add search functionality for mahasiswa by angkatan with nama or nim filtering and add functionality to retrieve all mahasiswa PA data by angkatan

--- src/controllers/dosen.controllers.ts
+++ src/controllers/dosen.controllers.ts
@@ -1,59 +1,106 @@
 import { PrismaClient } from "@prisma/client";
+import { response } from "express";
 
 const prisma = new PrismaClient();
 
-const getAllMahasiswaPAByAngkatan = async (req: any, res: any) => {
-    const { angkatan, nip, nim, nama } = req.query;
-    console.log(angkatan, nip, nim, nama); 
+const findMahasiswaByNameOrNim = async (req: any, res: any) => {
+  const { nama, nim, nip, angkatan } = req.params;
+
+  try {
+    const result = await prisma.$queryRaw`
+    SELECT nim, nama
+    FROM mahasiswa WHERE CONCAT('20', SUBSTRING(nim, 2, 2)) = ${angkatan}
+    AND nip = ${nip}
+    AND (nama LIKE ${`%${nama}%`} OR nim LIKE ${`%${nim}%`});
+    `;
+
     res.status(200).json({
-        response: true,
-        message: "list semua mahasiswa berdasar dosen pa",
+      response: true,
+      message: "Nama atau Nim mahasiswa",
+      data: result,
     });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({
+      response: false,
+      message: "Error retrieving data",
+      error: error,
+    });
+  }
+};
+
+const getAllMahasiswaPAByAngkatan = async (req: any, res: any) => {
+  const { angkatan, nip } = req.params;
+
+  try {
+    const result = await prisma.$queryRaw`
+    SELECT nim, nama, CONCAT('20', SUBSTRING(nim, 2, 2)) AS tahun
+    FROM mahasiswa
+    WHERE CONCAT('20', SUBSTRING(nim, 2, 2)) = ${angkatan} AND nip = ${nip}
+    `;
+
+    res.status(200).json({
+      response: true,
+      message: "Ini adalah list mahasiswa berdasarkan angkatan dan dosen pa",
+      data: result,
+    });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({
+      response: false,
+      message: "Error retrieving data",
+      error: error,
+    });
+  }
 };
 
 const getInfoMahasiswaPAPerAngkatanByNIP = async (req: any, res: any) => {
-    const { nip } = req.params;
+  const { nip } = req.params;
 
-    const result = await prisma.mahasiswa.groupBy({
-        by: ['nim'],
-        _count: {
-          nim: true,
-        },
-        orderBy: {
-          nim: 'desc',
-        },
-        where: {
-            nip: nip,
-        },
-        take: 8,
-      });
-      
-      // Langkah 2: Mengelompokkan hasil berdasarkan tahun dan menghitung jumlah
-      const groupedResult = result.reduce((acc, item) => {
-        const tahun = `20${item.nim.slice(1, 3)}`;
-        
-        if (acc[tahun]) {
-          acc[tahun] += item._count.nim;
-        } else {
-          acc[tahun] = item._count.nim;
-        }
-      
-        return acc;
-      }, {} as Record<string, number>);
-      
-      // Langkah 3: Mengonversi objek menjadi array dan mengurutkannya
-      const formattedResult = Object.keys(groupedResult)
-        .map((tahun) => ({
-          tahun,
-          jumlah: groupedResult[tahun],
-        }))
-        .sort((a, b) => b.tahun.localeCompare(a.tahun)); // Mengurutkan tahun secara menurun
+  const result = await prisma.mahasiswa.groupBy({
+    by: ["nim"],
+    _count: {
+      nim: true,
+    },
+    orderBy: {
+      nim: "desc",
+    },
+    where: {
+      nip: nip,
+    },
+    take: 8,
+  });
 
-    res.status(200).json({
-        response: true,
-        message: "list detail mahasiswa berdasar nim individu",
-        data: formattedResult
-    });
+  // Langkah 2: Mengelompokkan hasil berdasarkan tahun dan menghitung jumlah
+  const groupedResult = result.reduce((acc, item) => {
+    const tahun = `20${item.nim.slice(1, 3)}`;
+
+    if (acc[tahun]) {
+      acc[tahun] += item._count.nim;
+    } else {
+      acc[tahun] = item._count.nim;
+    }
+
+    return acc;
+  }, {} as Record<string, number>);
+
+  // Langkah 3: Mengonversi objek menjadi array dan mengurutkannya
+  const formattedResult = Object.keys(groupedResult)
+    .map((tahun) => ({
+      tahun,
+      jumlah: groupedResult[tahun],
+    }))
+    .sort((a, b) => b.tahun.localeCompare(a.tahun)); // Mengurutkan tahun secara menurun
+
+  res.status(200).json({
+    response: true,
+    message: "list detail mahasiswa berdasar nim individu",
+    data: formattedResult,
+  });
 };
 
-export { getAllMahasiswaPAByAngkatan, getInfoMahasiswaPAPerAngkatanByNIP }
+export {
+  getAllMahasiswaPAByAngkatan,
+  getInfoMahasiswaPAPerAngkatanByNIP,
+  findMahasiswaByNameOrNim,
+};
