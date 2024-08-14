766e3850ee49075db8a3ab8deba1087791e6cacd
DIKI-WAHYUDI-2 <muhammaddikiw2@gmail.com>
2024-08-14 09:21:07 +0700

    🔥 feat: add function to get Mahasiswa PA info per angkatan by NIP

--- src/controllers/dosen.controllers.ts
+++ src/controllers/dosen.controllers.ts
@@ -1,16 +1,59 @@
-const getAllMahasiswaPA = async (req: any, res: any) => {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getAllMahasiswaPAByAngkatan = async (req: any, res: any) => {
+    const { angkatan, nip, nim, nama } = req.query;
+    console.log(angkatan, nip, nim, nama); 
     res.status(200).json({
         response: true,
         message: "list semua mahasiswa berdasar dosen pa",
     });
 };
 
-const getMahasiswaPAByNIM = async (req: any, res: any) => {
-    const { nim } = req.params;
+const getInfoMahasiswaPAPerAngkatanByNIP = async (req: any, res: any) => {
+    const { nip } = req.params;
+
+    const result = await prisma.mahasiswa.groupBy({
+        by: ['nim'],
+        _count: {
+          nim: true,
+        },
+        orderBy: {
+          nim: 'desc',
+        },
+        where: {
+            nip: nip,
+        },
+        take: 8,
+      });
+      
+      // Langkah 2: Mengelompokkan hasil berdasarkan tahun dan menghitung jumlah
+      const groupedResult = result.reduce((acc, item) => {
+        const tahun = `20${item.nim.slice(1, 3)}`;
+        
+        if (acc[tahun]) {
+          acc[tahun] += item._count.nim;
+        } else {
+          acc[tahun] = item._count.nim;
+        }
+      
+        return acc;
+      }, {} as Record<string, number>);
+      
+      // Langkah 3: Mengonversi objek menjadi array dan mengurutkannya
+      const formattedResult = Object.keys(groupedResult)
+        .map((tahun) => ({
+          tahun,
+          jumlah: groupedResult[tahun],
+        }))
+        .sort((a, b) => b.tahun.localeCompare(a.tahun)); // Mengurutkan tahun secara menurun
+
     res.status(200).json({
         response: true,
         message: "list detail mahasiswa berdasar nim individu",
+        data: formattedResult
     });
 };
 
-export { getAllMahasiswaPA, getMahasiswaPAByNIM }
+export { getAllMahasiswaPAByAngkatan, getInfoMahasiswaPAPerAngkatanByNIP }
