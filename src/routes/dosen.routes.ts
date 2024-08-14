3ec1d2a87c0d9b8bb8ecdcf1de8add70d362cf28
DIKI-WAHYUDI-2 <muhammaddikiw2@gmail.com>
2024-08-14 23:04:15 +0700

    🔥 feat: minified routes for dosen endpoints

--- src/routes/dosen.routes.ts
+++ src/routes/dosen.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import accessTokenValidation from "../middlewares/auth.middlewares";
-import { findMahasiswaByNameOrNim, getAllMahasiswaPAByAngkatan, getInfoMahasiswaPAPerAngkatanByNIP } from "../controllers/dosen.controllers";
+import { findMahasiswaByNameOrNim, getInfoMahasiswaPAPerAngkatanByNIP, postSetoran } from "../controllers/dosen.controllers";
 import { dosenPaOnly } from "../middlewares/protected.middlewares";
 
 const router = express.Router();
 
 router.get("/dosen/mahasiswa/angkatan/info/:nip", accessTokenValidation, dosenPaOnly, getInfoMahasiswaPAPerAngkatanByNIP);
-router.get("/dosen/mahasiswa/:nip/:angkatan", accessTokenValidation, dosenPaOnly, getAllMahasiswaPAByAngkatan);
-router.get("/dosen/mahasiswa/find/:nip/:angkatan/:nama/:nim", accessTokenValidation, dosenPaOnly, findMahasiswaByNameOrNim);
+router.get("/dosen/mahasiswa", accessTokenValidation, dosenPaOnly, findMahasiswaByNameOrNim);
+router.post("/dosen/mahasiswa", accessTokenValidation, dosenPaOnly, postSetoran);
 
 export default router;
 
