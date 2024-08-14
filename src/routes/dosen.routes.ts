ad780a90cc6fd4b46cfb12d73a4957cd5abf8b33
DIKI-WAHYUDI-2 <muhammaddikiw2@gmail.com>
2024-08-14 17:00:02 +0700

    🔥 feat: implement RBAC to protect all dosen routes

--- src/routes/dosen.routes.ts
+++ src/routes/dosen.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import accessTokenValidation from "../middlewares/auth.middlewares";
-import { getAllMahasiswaPAByAngkatan, getInfoMahasiswaPAPerAngkatanByNIP } from "../controllers/dosen.controllers";
+import { findMahasiswaByNameOrNim, getAllMahasiswaPAByAngkatan, getInfoMahasiswaPAPerAngkatanByNIP } from "../controllers/dosen.controllers";
 import { dosenPaOnly } from "../middlewares/protected.middlewares";
 
 const router = express.Router();
 
 router.get("/dosen/mahasiswa/angkatan/info/:nip", accessTokenValidation, dosenPaOnly, getInfoMahasiswaPAPerAngkatanByNIP);
-router.get("/dosen/mahasiswa", accessTokenValidation, dosenPaOnly, getAllMahasiswaPAByAngkatan);
+router.get("/dosen/mahasiswa/:nip/:angkatan", accessTokenValidation, dosenPaOnly, getAllMahasiswaPAByAngkatan);
+router.get("/dosen/mahasiswa/find/:nip/:angkatan/:nama/:nim", accessTokenValidation, dosenPaOnly, findMahasiswaByNameOrNim);
 
 export default router;
 
