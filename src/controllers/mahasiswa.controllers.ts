08383cc18f0671fc4aa06ef94ee2d0dbe81e2493
Farhan Fadhila <115599443+FARHANFADHILA@users.noreply.github.com>
2024-08-17 19:35:35 +0700

    Revert "feat for generating data setoran for stats in mahasiswa roles at fe"

--- src/controllers/mahasiswa.controllers.ts
+++ src/controllers/mahasiswa.controllers.ts
@@ -2,11 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
-interface statsInfoSetoranMahasiswaProps {
+interface exmaple {
 	label: string;
-	jumlah_wajib_setor: number;
-	jumlah_sudah_setor: number;
-	jumlah_belum_setor: number;
+	jumlah_setor: string;
+	total_surah: string;
 	persentase: string;
 }
 
@@ -14,15 +13,15 @@ const getInfoMahasiswaByEmail = async (req: Request, res: Response) => {
 	const { email } = req.params;
 	try {
 		// Mengambil data nama, dan nim mahasiswa berdasarkan email
-		const result = await prisma.mahasiswa.findFirst({
-			where: {
-				email: email,
-			},
-			select: {
-				nama: true,
-				nim: true,
-			},
-		});
+    const result = await prisma.mahasiswa.findFirst({
+      where: {
+        email: email,
+      },
+      select: {
+        nama: true,
+        nim: true,
+      },
+    });		
 		res.status(200).json({
 			response: true,
 			message: "data mahasiswa",
@@ -42,41 +41,47 @@ const getInfoSetoranMahasiswaByNIM = async (req: Request, res: Response) => {
 	try {
 		// Mengambil data menggunakan query SQL mentah
 		const result = await prisma.$queryRaw`
-			SELECT 
-				surah.label "label",
-				COUNT(*) "jumlah_wajib_setor",
-				COUNT(setoran.id) "jumlah_sudah_setor",
-				(COUNT(*)::numeric - COUNT(setoran.id)::numeric) AS "jumlah_belum_setor",
-				CONCAT(
-					ROUND(
-					(COUNT(setoran.id)::numeric / 
-						COUNT(*)::numeric
-					) * 100, 2
-					)::text,
-					'%'
-				) AS "persentase"
-			FROM
-				surah
-			LEFT JOIN
-				setoran ON setoran.nomor_surah = surah.nomor AND setoran.nim = ${nim}
-			GROUP BY
-				surah.label
-			ORDER BY
-				CASE
-					WHEN surah.label = 'KP' THEN 1
-					WHEN surah.label = 'SEMKP' THEN 2
-					WHEN surah.label = 'DAFTAR_TA' THEN 3
-					WHEN surah.label = 'SEMPRO' THEN 4
-					WHEN surah.label = 'SIDANG_TA' THEN 5
-					ELSE 6
-				END;
-		`;
+      SELECT 
+        SURAH.LABEL AS "label", 
+        COUNT(SETORAN.ID) AS "jumlah_setor",
+        CASE 
+          WHEN SURAH.LABEL = 'KP' THEN 8
+          WHEN SURAH.LABEL = 'SEMKP' THEN 8
+          WHEN SURAH.LABEL = 'DAFTAR_TA' THEN 6
+          WHEN SURAH.LABEL = 'SEMPRO' THEN 12
+          WHEN SURAH.LABEL = 'SIDANG_TA' THEN 3
+          ELSE 0 
+        END AS "total_surah",
+        CONCAT(
+          ROUND(
+            (COUNT(SETORAN.ID)::numeric / 
+             CASE 
+               WHEN SURAH.LABEL = 'KP' THEN 8
+               WHEN SURAH.LABEL = 'SEMKP' THEN 8
+               WHEN SURAH.LABEL = 'DAFTAR_TA' THEN 6
+               WHEN SURAH.LABEL = 'SEMPRO' THEN 12
+               WHEN SURAH.LABEL = 'SIDANG_TA' THEN 3
+               ELSE 0 
+             END
+            ) * 100, 2
+          )::text,
+          '%'
+        ) AS "persentase"
+      FROM 
+        SURAH
+      LEFT JOIN 
+        SETORAN ON SETORAN.NOMOR_SURAH = SURAH.NOMOR AND SETORAN.NIM = ${nim}
+      GROUP BY 
+        SURAH.LABEL
+      ORDER BY 
+        SURAH.LABEL;
+    `;
 
-		const resultWithNumbers = (result as any).map((item: statsInfoSetoranMahasiswaProps) => ({
+		console.log("ini ", nim);
+		const resultWithNumbers = (result as any).map((item: exmaple) => ({
 			label: item.label,
-			jumlah_wajib_setor: Number(item.jumlah_wajib_setor),
-			jumlah_sudah_setor: Number(item.jumlah_sudah_setor),
-			jumlah_belum_setor: Number(item.jumlah_belum_setor),
+			jumlah: Number(item.jumlah_setor),
+			total: Number(item.total_surah),
 			persentase: item.persentase,
 		}));
 
@@ -121,6 +126,8 @@ const getSurahMahasiswaByNIM = async (req: any, res: any) => {
 			},
 		},
 	});
+
+	
 
 	res.status(200).json({
 		response: true,
