4da63f7b82b9b332c2f2bd334b17caf3f0204acf
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-17 14:43:24 +0700

    🔥 feat: adding new routes for getMe() in mhs role

--- src/routes/mahasiswa.routes.ts
+++ src/routes/mahasiswa.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import accessTokenValidation from "../middlewares/auth.middlewares";
-import { getInfoSetoranMahasiswaByNIM, getSurahMahasiswaByNIM } from "../controllers/mahasiswa.controllers";
+import { getInfoMahasiswaByEmail, getInfoSetoranMahasiswaByNIM, getSurahMahasiswaByNIM } from "../controllers/mahasiswa.controllers";
 import { mahasiswaOnly } from "../middlewares/protected.middlewares";
 
 const router = express.Router();
 
+router.get("/mahasiswa/info/:email", accessTokenValidation, mahasiswaOnly, getInfoMahasiswaByEmail);
 router.get("/mahasiswa/surah/info/:nim", accessTokenValidation, mahasiswaOnly, getInfoSetoranMahasiswaByNIM);
 router.get("/mahasiswa/surah/:nim", accessTokenValidation, mahasiswaOnly, getSurahMahasiswaByNIM);
 
