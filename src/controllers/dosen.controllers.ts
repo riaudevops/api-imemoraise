7e264cf80e455c400532281105a4e0aeb89dbf09
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-26 23:32:59 +0700

    💎 style: formatting code

--- src/controllers/dosen.controllers.ts
+++ src/controllers/dosen.controllers.ts
@@ -1,161 +1,162 @@
 import { PrismaClient } from "@prisma/client";
 import {
-	getAllInfoSetoranByNip,
-	getInfoMahasiswaPAPerAngkatanByNIP,
-	IDGenerator,
+  getAllInfoSetoranByNip,
+  getInfoMahasiswaPAPerAngkatanByNIP,
+  IDGenerator,
 } from "../services/dosen.services";
 import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
 const getInfoDosenByEmail = async (req: Request, res: Response) => {
-	const { email } = req.params;
-	try {
-		// Mengambil data nama, dan nim mahasiswa berdasarkan email
-		const resultInfoPribadiDosen = await prisma.dosen.findFirst({
-			where: {
-				email: email,
-			},
-			select: {
-				nama: true,
-				nip: true,
-			},
-		});
+  const { email } = req.params;
 
-		const resultInfoMahasiswaPerAngkatan =
-			await getInfoMahasiswaPAPerAngkatanByNIP(resultInfoPribadiDosen!.nip);
+  try {
+    // Mengambil data nama, dan nim mahasiswa berdasarkan email
+    const resultInfoPribadiDosen = await prisma.dosen.findFirst({
+      where: {
+        email: email,
+      },
+      select: {
+        nama: true,
+        nip: true,
+      },
+    });
 
-		const statsSetoran = await getAllInfoSetoranByNip(
-			resultInfoPribadiDosen!.nip
-		);
+    const resultInfoMahasiswaPerAngkatan =
+      await getInfoMahasiswaPAPerAngkatanByNIP(resultInfoPribadiDosen!.nip);
 
-		res.status(200).json({
-			response: true,
-			message:
-				"Berikut info dosen lengkap serta info mahasiswa per angkatan (max 8 akt)!",
-			data: {
-				nama: resultInfoPribadiDosen!.nama,
-				nip: resultInfoPribadiDosen!.nip,
-				info: resultInfoMahasiswaPerAngkatan,
-				stats: {
-					list_setoran_perhari: statsSetoran,
-				},
-			},
-		});
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({
-			response: false,
-			message: "internal server error",
-		});
-	}
+    const statsSetoran = await getAllInfoSetoranByNip(
+      resultInfoPribadiDosen!.nip
+    );
+
+    res.status(200).json({
+      response: true,
+      message:
+        "Berikut info dosen lengkap serta info mahasiswa per angkatan (max 8 akt)!",
+      data: {
+        nama: resultInfoPribadiDosen!.nama,
+        nip: resultInfoPribadiDosen!.nip,
+        info: resultInfoMahasiswaPerAngkatan,
+        stats: {
+          list_setoran_perhari: statsSetoran,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      response: false,
+      message: "Internal server error!",
+    });
+  }
 };
 
 const postSetoran = async (req: Request, res: Response) => {
-	const { nim, nip, nomor_surah, tgl_setoran } = req.body;
+  const { nim, nip, nomor_surah, tgl_setoran } = req.body;
 
-	// Validasi input
-	if (!nim || !nip || !nomor_surah) {
-		return res.status(400).json({
-			response: false,
-			message: "Waduh, lengkapi dulu datanya mas!",
-		});
-	}
+  // Validasi input
+  if (!nim || !nip || !nomor_surah) {
+    return res.status(400).json({
+      response: false,
+      message: "Waduh, lengkapi dulu datanya mas!",
+    });
+  }
 
-	// Convert nomor_surah to integer if it is a string, and returns err if not
-	const nomorSurahInt = parseInt(nomor_surah as string, 10);
-	if (isNaN(nomorSurahInt)) {
-		return res.status(400).json({
-			response: false,
-			message: "Waduh, nomor surah-nya salah format mas!",
-		});
-	}
+  // Convert nomor_surah to integer if it is a string, and returns err if not
+  const nomorSurahInt = parseInt(nomor_surah as string, 10);
+  if (isNaN(nomorSurahInt)) {
+    return res.status(400).json({
+      response: false,
+      message: "Waduh, nomor surah-nya salah format mas!",
+    });
+  }
 
-	await prisma.$transaction(async (prisma) => {
-		try {
-			// Periksa apakah kombinasi nim, nip, dan nomor_surah sudah ada (antisipasi duplikasi setoran di 1 mhs)
-			const existingSetoran = await prisma.setoran.findFirst({
-				where: {
-					AND: [
-						{ nim: nim as string },
-						{ nip: nip as string },
-						{ nomor_surah: nomorSurahInt },
-					],
-				},
-			});
-			if (existingSetoran) {
-				return res.status(400).json({
-					response: false,
-					message: "Maaf, mahasiswa ybs telah menyetor surah tersebut!",
-				});
-			}
+  await prisma.$transaction(async (prisma) => {
+    try {
+      // Periksa apakah kombinasi nim, nip, dan nomor_surah sudah ada (antisipasi duplikasi setoran di 1 mhs)
+      const existingSetoran = await prisma.setoran.findFirst({
+        where: {
+          AND: [
+            { nim: nim as string },
+            { nip: nip as string },
+            { nomor_surah: nomorSurahInt },
+          ],
+        },
+      });
+      if (existingSetoran) {
+        return res.status(400).json({
+          response: false,
+          message: "Maaf, mahasiswa ybs telah menyetor surah tersebut!",
+        });
+      }
 
-			// Generate ID setoran baru, format SH240001 ++
-			const idSetoran = await IDGenerator.generateNewIdSetoran();
+      // Generate ID setoran baru, format SH240001 ++
+      const idSetoran = await IDGenerator.generateNewIdSetoran();
 
-			// Simpan data ke database mas
-			await prisma.setoran.create({
-				data: {
-					id: idSetoran,
-					tgl_setoran: tgl_setoran ? new Date(tgl_setoran) : new Date(),
-					tgl_validasi: new Date(),
-					nim: nim as string,
-					nip: nip as string,
-					nomor_surah: nomorSurahInt,
-				},
-			});
+      // Simpan data ke database mas
+      await prisma.setoran.create({
+        data: {
+          id: idSetoran,
+          tgl_setoran: tgl_setoran ? new Date(tgl_setoran) : new Date(),
+          tgl_validasi: new Date(),
+          nim: nim as string,
+          nip: nip as string,
+          nomor_surah: nomorSurahInt,
+        },
+      });
 
-			// Kirim respons sukses
-			res.status(201).json({
-				response: true,
-				message: "Yeay, proses validasi setoran berhasil! ✨",
-			});
-		} catch (error) {
-			res.status(500).json({
-				response: false,
-				message: "Oops! ada kesalahan di server kami 😭",
-			});
-		}
-	});
+      // Kirim respons sukses
+      res.status(201).json({
+        response: true,
+        message: "Yeay, proses validasi setoran berhasil! ✨",
+      });
+    } catch (error) {
+      res.status(500).json({
+        response: false,
+        message: "Oops! ada kesalahan di server kami 😭",
+      });
+    }
+  });
 };
 
 const deleteSetoranByID = async (req: Request, res: Response) => {
-	const { id_setoran } = req.params;
+  const { id_setoran } = req.params;
 
-	// Validasi input
-	if (!id_setoran) {
-		return res.status(400).json({
-			response: false,
-			message: "Waduh, id setoran-nya kagak ada mas, apa yang mau diapus!",
-		});
-	}
+  // Validasi input
+  if (!id_setoran) {
+    return res.status(400).json({
+      response: false,
+      message: "Waduh, id setoran-nya kagak ada mas, apa yang mau diapus!",
+    });
+  }
 
-	try {
-		await prisma.setoran
-			.delete({
-				where: {
-					id: id_setoran,
-				},
-			})
-			.then(() => {
-				return res.status(200).json({
-					response: true,
-					message: "Yeay, data setoran berhasil di-batalkan! ✨",
-				});
-			});
-	} catch (error) {
-		return res.status(500).json({
-			response: false,
-			message: "Oops! ada kesalahan di server kami 😭",
-		});
-	}
+  try {
+    await prisma.setoran
+      .delete({
+        where: {
+          id: id_setoran,
+        },
+      })
+      .then(() => {
+        return res.status(200).json({
+          response: true,
+          message: "Yeay, data setoran berhasil di-batalkan! ✨",
+        });
+      });
+  } catch (error) {
+    return res.status(500).json({
+      response: false,
+      message: "Oops! ada kesalahan di server kami 😭",
+    });
+  }
 };
 
 const findMahasiswaByNameOrNim = async (req: Request, res: Response) => {
-	const { search, nip, angkatan } = req.query;
+  const { search, nip, angkatan } = req.query;
 
-	try {
-		const result = await prisma.$queryRaw`
+  try {
+    const result = await prisma.$queryRaw`
 			SELECT 
 				nim, nama
 			FROM 
@@ -166,22 +167,22 @@ const findMahasiswaByNameOrNim = async (req: Request, res: Response) => {
 				AND (LOWER(nama) LIKE ${`%${search}%`} OR LOWER(nim) LIKE ${`%${search}%`});
 		`;
 
-		res.status(200).json({
-			response: true,
-			message: "Berikut list data mahasiswa yang sesuai!",
-			data: result,
-		});
-	} catch (error) {
-		res.status(500).json({
-			response: false,
-			message: "Oops! ada kesalahan di server kami 😭",
-		});
-	}
+    res.status(200).json({
+      response: true,
+      message: "Berikut list data mahasiswa yang sesuai!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      response: false,
+      message: "Oops! ada kesalahan di server kami 😭",
+    });
+  }
 };
 
 export {
-	getInfoDosenByEmail,
-	findMahasiswaByNameOrNim,
-	postSetoran,
-	deleteSetoranByID,
+  getInfoDosenByEmail,
+  findMahasiswaByNameOrNim,
+  postSetoran,
+  deleteSetoranByID,
 };
--- src/middlewares/protected.middlewares.ts
+++ src/middlewares/protected.middlewares.ts
@@ -2,22 +2,20 @@ import { Request, Response, NextFunction } from "express";
 
 // Extend Request type to include 'role'
 interface CustomRequest extends Request {
-    roles?: string[];
+  roles?: string[];
 }
 
 const authorizeRoles = (...roles: string[]) => {
   return (req: CustomRequest, res: Response, next: NextFunction) => {
-    const userRole =(req as CustomRequest).roles;
+    const userRole = (req as CustomRequest).roles;
     const hasRole = userRole?.some((role) => roles.includes(role));
 
     if (!hasRole) {
-      return res.status(403).json({ message: 'Forbidden: Insufficient role' });
+      return res.status(403).json({ message: "Forbidden: Insufficient role" });
     }
 
     next();
   };
 };
 
-export {
-    authorizeRoles
-}
+export { authorizeRoles };
