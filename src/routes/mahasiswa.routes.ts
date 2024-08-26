493332eaa19a8aa2361758bdc6dead80e5a94501
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-26 11:15:03 +0700

    ✨ perf: adjust new routes for new controller implements

--- src/routes/mahasiswa.routes.ts
+++ src/routes/mahasiswa.routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import accessTokenValidation from "../middlewares/auth.middlewares";
-import { getInfoMahasiswaByEmail, getInfoSetoranMahasiswaByNIM, getSetoranMahasiswaByNIM } from "../controllers/mahasiswa.controllers";
+import { getInfoMahasiswaByEmail, getInfoSetoranMahasiswaByNIM, getAllSetoranMahasiswaByNIM } from "../controllers/mahasiswa.controllers";
 import { authorizeRoles } from "../middlewares/protected.middlewares";
 
 const router = express.Router();
 
 router.get("/mahasiswa/info/:email", accessTokenValidation, authorizeRoles("mahasiswa"), getInfoMahasiswaByEmail);
-router.get("/mahasiswa/setoran/:nim", accessTokenValidation, authorizeRoles("mahasiswa", "dosen-pa"), getSetoranMahasiswaByNIM);
+router.get("/mahasiswa/setoran/:nim", accessTokenValidation, authorizeRoles("mahasiswa", "dosen-pa"), getAllSetoranMahasiswaByNIM);
 router.get("/mahasiswa/setoran/info/:nim", accessTokenValidation, authorizeRoles("mahasiswa"), getInfoSetoranMahasiswaByNIM);
 
 export default router;
